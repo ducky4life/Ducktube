929edc7fd37097e8bfb99208efb2ba9a23a514fb
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-07-10 04:44:25 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@
 # for my development use
 git pull
 docker stop ducktube
-docker build -t ducktube:latest -f Dockerfile .
+docker build -t ducktube:latest -f arm64.Dockerfile .
 docker run --rm -p 8080:8080 --name ducktube ducktube:latest
